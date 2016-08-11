418998a6bfd1825742b6c82d6f6a792854f355ff
maksr51314 <maksr51314@gmail.com>
2016-08-11 09:59:06 +0300

    Added react to the project

--- javascript/Main.js
+++ javascript/Main.js
@@ -1,6 +1,10 @@
 class Main {
 
     constructor() {
+
+
+        //var react = new ReactMain();
+
         var firebaseconfig = {
             apiKey: "AIzaSyDEZBo3lOJaG_7Va6M2CiQGDSCCngs4YBk",
             authDomain: "itoffices-3fa8a.firebaseapp.com",
